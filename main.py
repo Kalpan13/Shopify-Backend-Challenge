2921fa8528ad9a4f56bd783b58d974d8db6a4b7d
Kalpan13 <kalpan2713@gmail.com>
2022-01-19 21:26:09 -0500

    Deployment Changes

--- main.py
+++ main.py
@@ -41,4 +41,5 @@ items_ns.add_resource(InventoryItemList, "")
 if __name__ == '__main__':
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(threaded=True)
+    
